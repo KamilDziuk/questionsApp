db1918d6c7073f8f70b8a7e5317b4e75a8d9aee2
kamildziuk <kontakt@kamildziuk.pl>
2024-05-31 10:02:53 +0200

    Improving code readability(JS)

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<script src="js/script.js" defer>
-  </script>
+<script src="js/script.js" defer></script>
 <!-- path to google translate start -->
 <script type="text/javascript"
 src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
@@ -31,7 +30,7 @@ Number of total questions: <div class="lengthApp"></div>
 <!-- form for adding questions and answers end -->
 
 <!-- View questions and answers start -->
-<button class="refresh">Questions</button><br><br>
+<button class="questions">Questions</button><br><br>
 <button class="answer">Answer</button><br><br>
 <div class="contentOne"></div><br>
 <div class="contentTwo"></div><br><br>
@@ -42,18 +41,151 @@ Before adding another item, you must enter your password <input class="passwordI
 <?php
 require_once 'configFunction.php';
 ?>
+
 <script>
- //  Downloading data from the database start  
-let questionsApp = <?php echo json_encode($questionsApp);?> 
-//  Downloading data from the database end
+let questionsApp = <?php echo json_encode($questionsApp);?>;
+</script>
+<script>
+
+
+
+function QuestionAndAnswerApp()
+{
+this.id = document.querySelector('.id');
+this.questionsEN = document.querySelector('.questionsEN');
+this.answerTheQuestionEN = document.querySelector('.answerTheQuestionEN');
+this.submit = document.querySelector('.submit');
+this.contentOne = document.querySelector('.contentOne');
+this.contentTwo = document.querySelector('.contentTwo');  
+this.questions = document.querySelector('.questions');
+this.answer = document.querySelector('.answer');
+this.lengthApp = document.querySelector('.lengthApp');
+this._questionsApp = <?php echo json_encode($questionsApp);?>;
+this.lengthApp = document.querySelector('.lengthApp');
+this.passwordAlert = document.querySelector('.passwordAlert');
+this.passwordInput = document.querySelector('.passwordInput');
+
+this.sendContent = () =>
+{
+
+  this.submit.addEventListener('click', () =>
+  {
+      this.contentQuestionsApp();
+  })
+};
+
+this.contentQuestionsApp = () =>
+{
+  if(this.passwordInput.value == "")
+    {
+      this.passwordAlert.style.color = "red";
+      this. passwordAlert.innerHTML = "You have not entered your password or you entered the wrong password";
+    }
+    else if (this.passwordInput.value == ""){
+      this.passwordAlert.style.color = "green";
+      this.passwordAlert.innerHTML = `Password correct! <br> You have added a new pose from the list`;
+        
+    
+  let questionsAppData = {
+id: this.id.value,
+questionsEN: this.questionsEN.value,
+answerTheQuestionEN: this.answerTheQuestionEN.value
+};
+let formData = new FormData();
+for(let key in questionsAppData) {
+formData.append(key, questionsAppData[key]);
+}
+ fetch('refreshment.php', {
+method: 'POST',
+body: formData
+})
+
+.then(response => response.text())
+.then(data => {
+
+console.log(data);
+})
+.catch(error => {
+console.error('Error:', error);
+});
+}
+    else{
+        this.passwordAlert.style.color = "red";
+        this.passwordAlert.innerHTML = "You have not entered your password or you entered the wrong password";
+    }
+}
+
+this.lengthTable = () =>
+{
+  return this.lengthApp.innerHTML = this._questionsApp.length + 3;   
+};
+
+this.questionButton = () =>
+{
+ 
+
+  this.questions.addEventListener('click', () =>
+{  
+  if(this.contentOne.style.display == "none")
+  {
+  this.drawStart = () =>{
+  this.draw = this._questionsApp[Math.floor(Math.random() * this._questionsApp.length)];
+   return this.draw.questionsEN;
+  }
+
+ 
+    this.contentOne.style.display = "block";
+    this.contentOne.innerHTML =  this.drawStart();
+  }
+  else
+  {
+    this.contentOne.style.display = "none";
+  }
+ 
+     
+})
+};
+
+
+
+this.answerButton = () =>
+{
+
+
+
+  this.answer.addEventListener('click', () =>
+{
+  if(this.contentTwo.style.display == "none")
+  {
+    this.contentTwo.style.display = "block";
+  this.questionButton();
+      this.contentTwo.innerHTML = this.draw.answerTheQuestionEN;
+  }
+  else
+  {
+    this.contentTwo.style.display = "none";
+  }
+
+})
+};
+
+}
+
+let resultsQuestionsApp = new QuestionAndAnswerApp();
+
+resultsQuestionsApp.lengthTable();
+resultsQuestionsApp.questionButton();
+resultsQuestionsApp.answerButton();
+resultsQuestionsApp.sendContent();
+
+
 </script>
 
  <!-- Search function start  -->
 
  <div class="search-wrapper">
     <label for="search">Search </label><br>
-    <input type="search" id="search" data-search>
-  </div>
+    <input type="search" id="search" data-search></div>
   <div class="questions-cards" data-questions-cards-container></div>
   <template data-questions-template>
     <div class="card">
--- js/script.js
+++ js/script.js
@@ -1,160 +1,11 @@
 
-
-let lengthApp = document.querySelector('.lengthApp');
-let questionsEN = document.querySelector('.questionsEN');
-let answerTheQuestionEN = document.querySelector('.answerTheQuestionEN');
-let passwordInput = document.querySelector('.passwordInput');
-let refresh = document.querySelector('.refresh');
-let answer = document.querySelector('.answer');
-let contentOne = document.querySelector('.contentOne');
-let contentTwo = document.querySelector('.contentTwo');
-let contentQuestionEN = document.querySelector('.contentQuestionEN');
-let id = document.querySelector('.id');
-let passwordAlert = document.querySelector('.passwordAlert');
-
-
-
-
-// let array1 = [1, 2, 3];
-
-// console.log(array1.includes(2));
-
-
-// let pets = ['cat', 'dog', 'bat'];
-
-// console.log(pets.includes('cat'));
-
-
-// console.log(pets.includes('at'));
-
-
-
-// search.addEventListener("input", e => {
-
-// let value = e.target.value
-
-
-
-// let reservationTimesRes = questionsApp.filter(questionsApp =>   questionsApp.id == 1 );
-
-
-
-
-//     reservationTimesRes.forEach(reservationTimesRes =>
-//   {
-//     let reservationTimesResOneAll = JSON.stringify(reservationTimesRes.questionsEN).toLowerCase()
-   
-
-//     test.innerHTML = ""
-
-//         if (  reservationTimesResOneAll.includes(value))
-//         {
-//             test.innerHTML = reservationTimesResOneAll
-//         }
-    
-        
-//         else
-//         {
-//             test.innerHTML = ""
-//         }
-// })
-
-
-// })
-
-
-
-
-
-
-
-
-
-// let value = e.target.value
-
-
-
-
-
-
-
-// Method post nad password encryption start
-document.querySelector('.submit').addEventListener('click', function() {
-    if(passwordInput.value == "")
-    {
-    passwordAlert.style.color = "red";
-    passwordAlert.innerHTML = "You have not entered your password or you entered the wrong password";
-    }
-    else if (passwordInput.value == "Laramakota1212"){
-        passwordAlert.style.color = "green";
-        passwordAlert.innerHTML = `Password correct! <br> You have added a new pose from the list`;
-        
-    
-let questionsAppData = {
-id: id.value,
-questionsEN: questionsEN.value,
-answerTheQuestionEN: answerTheQuestionEN.value
-};
-let formData = new FormData();
-for(let key in questionsAppData) {
-formData.append(key, questionsAppData[key]);
-}
- fetch('refreshment.php', {
-method: 'POST',
-body: formData
-})
-.then(response => response.text())
-.then(data => {
-
-console.log(data);
-})
-.catch(error => {
-console.error('Error:', error);
-});
-    }
-    else{
-        passwordAlert.style.color = "red";
-        passwordAlert.innerHTML = "You have not entered your password or you entered the wrong password";
-    }
-});
-
-// Method post nad password encryption end
-
 //  function  to google translate start
  googleTranslateElementInit = () =>  {
 new google.translate.TranslateElement( 'google_translate_element');
 }
 // function to google translate end
 
-//question and answer randomization functione start
-let randomQuestion;
-lengthApp.innerHTML = questionsApp.length + 3;
-randomQuestionFunction = () =>
-{
-
-refresh.addEventListener('click', function() {   
-randomQuestion = questionsApp[Math.floor(Math.random() * questionsApp.length)];
-contentOne.innerHTML = randomQuestion.questionsEN;
-
-}) 
-};
-questionFunction = () =>
-randomQuestionFunction();
-{
-answer.addEventListener('click', function() {
-contentTwo.innerHTML = randomQuestion.answerTheQuestionEN;
-}); 
-}
-questionFunction();
-//question and answer randomization functione end
-
-
-
-
-
-  /* Search function start */
-
-  let search = document.querySelector(".search-wrapper")
+let search = document.querySelector(".search-wrapper")
 let userCardTemplate = document.querySelector("[data-questions-template]")
 let userCardContainer = document.querySelector("[data-questions-cards-container]")
 let searchInput = document.querySelector("[data-search]")
@@ -175,10 +26,12 @@ searchInput.addEventListener("input", e => {
     // Hide those items that do not match the given value start
     questions.element.classList.toggle("hide", !isVisible)
        // Hide those items that do not match the given value end
-   
-  })
+    
+      })
 
 })
+
+
 
 // card.style.display = "none";
     questions = questionsApp.map(questions => {
@@ -190,9 +43,14 @@ searchInput.addEventListener("input", e => {
       header.textContent = questions.questionsEN
       body.textContent = questions.answerTheQuestionEN
       // display downloaded values start
-      userCardContainer.append(card)
+       let test23 = userCardContainer.append(card)
+       test23;
       // display downloaded values end
 
       return { questionsEN: questions.questionsEN, answerTheQuestionEN: questions.answerTheQuestionEN, element: card }
+      
     })
+  
+
+
     /* Search function end */
